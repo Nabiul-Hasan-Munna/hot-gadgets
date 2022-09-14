78a0477c4d662165c43d60f7d93f89e0c1588438
Nabiul-Hasan-Munna <nabiulhasan.007@gmail.com>
2022-09-14 02:58:57 -0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(".banner-wrapper").slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+  });
+  
